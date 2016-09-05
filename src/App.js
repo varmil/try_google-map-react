0fa04ab0a4e2ad6203eabc0d5dcb42781ac60c8a
山本　晃大 <cf0293@CYGAMES-1818.local>
2016-09-05 12:41:37 +0900

    create google-map-simple-example

--- src/App.js
+++ src/App.js
@@ -1,22 +1,32 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
-import env  from './.env'
+import React, { Component } from 'react'
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+import './App.css'
+import env  from './.env'
+import GoogleMap from 'google-map-react'
+import MyGreatPlace from './components/my_great_place'
+
+const URL_KEYS = {
+  key: env.GMAP_API_KEY
 }
 
-export default App;
+export default class SimpleMapPage extends Component {
+  static defaultProps = {
+    center: {lat: 59.938043, lng: 30.337157},
+    zoom: 9,
+    greatPlaceCoords: {lat: 59.724465, lng: 30.080121}
+  }
+
+  render() {
+    return (
+      <div className={`container`}>
+         <GoogleMap
+          bootstrapURLKeys={URL_KEYS}
+          defaultCenter={this.props.center}
+          defaultZoom={this.props.zoom}>
+          <MyGreatPlace lat={59.955413} lng={30.337844} text={'A'} /* Kreyser Avrora */ />
+          <MyGreatPlace {...this.props.greatPlaceCoords} text={'B'} /* road circle */ />
+        </GoogleMap>
+      </div>
+    )
+  }
+}
